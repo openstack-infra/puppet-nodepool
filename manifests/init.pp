999361905a00fc28523a531187688bf66b0f77ef
Monty Taylor <mordred@inaugust.com>
2015-03-25 10:28:38 -0400

    Consume puppet-diskimage_builder
    Rather than having the diskimage-builder config sprinkled in here,
    it's been split into its own module. Consume that module.
    
    Depends-On: Idaac8b1e2ff3e3705bd50b76e48f5d12b743244c
    Change-Id: I822ddd1041c1207a3d82d4864cf020d4db7ccc43

--- manifests/init.pp
+++ manifests/init.pp
@@ -43,13 +43,6 @@ class nodepool (
     }
   }
 
-  # required by the nodepool diskimage-builder element scripts
-  if ! defined(Package['python-yaml']) {
-    package { 'python-yaml':
-      ensure => present,
-    }
-  }
-
   class { 'mysql::server':
     config_hash => {
       'root_password'  => $mysql_root_password,
@@ -77,25 +70,12 @@ class nodepool (
     'build-essential',
     'libffi-dev',
     'libssl-dev',
-    'kpartx',
-    'qemu-utils',
     'libgmp-dev',         # transitive dep of paramiko
-    # debootstrap is needed for building Debian images
-    'debootstrap',
-    'debian-keyring',
-    'ubuntu-keyring',
-    'vhd-util',
-    'yum',
-    'yum-utils',
-    'python-lzma',
   ]
 
   package { $packages:
     ensure  => present,
-    require => Apt::Ppa['ppa:openstack-ci-core/vhd-util'],
   }
-
-  apt::ppa { 'ppa:openstack-ci-core/vhd-util': }
 
   file { '/etc/mysql/conf.d/max_connections.cnf':
     ensure  => present,
@@ -125,14 +105,7 @@ class nodepool (
     source   => $git_source_repo,
   }
 
-  package { 'diskimage-builder':
-    ensure   => latest,
-    provider => pip,
-    require  => [
-      Class['pip'],
-      Package['python-yaml'],
-    ],
-  }
+  include diskimage_builder
 
   include pip
   exec { 'install_nodepool' :
