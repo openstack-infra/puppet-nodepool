a804b0eea5d2eb6ecf29784473c6bfb24074291d
Spencer Krum <nibz@cat.pdx.edu>
2015-10-28 18:28:03 -0700

    Nodepool image build workers
    Nodepool is splitting out builder-workers so that node builds can run in
    parallel and the nodepool use won't need sudo. This patch creates a new
    nodepool::install class that the main nodepool and builder classes use.
    
    Change-Id: I295e14d7494d859108a0e247c8106a9b673c629d

---
+++ manifests/install.pp
@@ -0,0 +1,127 @@
+# Copyright 2012-2013 Hewlett-Packard Development Company, L.P.
+# Copyright 2012 Antoine "hashar" Musso
+# Copyright 2012 Wikimedia Foundation Inc.
+# Copyright 2013 OpenStack Foundation
+# Copyright 2015 2015 IBM
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+# == Class: nodepool::install
+#
+class nodepool::install (
+  $revision = 'master',
+  $git_source_repo = 'https://git.openstack.org/openstack-infra/nodepool',
+) {
+
+  $packages = [
+    'build-essential',
+    'libffi-dev',
+    'libssl-dev',
+    'libgmp-dev',         # transitive dep of paramiko
+    # xml2 and xslt are needed to build python lxml.
+    'libxml2-dev',
+    'libxslt-dev',
+  ]
+
+  package { $packages:
+    ensure  => present,
+  }
+
+  user { 'nodepool':
+    ensure     => present,
+    home       => '/home/nodepool',
+    shell      => '/bin/bash',
+    gid        => 'nodepool',
+    managehome => true,
+    require    => Group['nodepool'],
+  }
+
+  group { 'nodepool':
+    ensure => present,
+  }
+
+  include ::diskimage_builder
+
+  vcsrepo { '/opt/nodepool':
+    ensure   => latest,
+    provider => git,
+    revision => $revision,
+    source   => $git_source_repo,
+  }
+
+
+  include ::pip
+  exec { 'install_nodepool' :
+    command     => 'pip install -U /opt/nodepool',
+    path        => '/usr/local/bin:/usr/bin:/bin/',
+    refreshonly => true,
+    subscribe   => Vcsrepo['/opt/nodepool'],
+    require     => [
+      Class['pip'],
+      Package['build-essential'],
+      Package['libffi-dev'],
+      Package['libssl-dev'],
+      Package['libxml2-dev'],
+      Package['libxslt-dev'],
+      Package['libgmp-dev'],
+    ],
+  }
+
+  file { '/etc/nodepool':
+    ensure => directory,
+  }
+
+  # used for storage of d-i-b images in non-ephemeral partition
+  file { '/opt/nodepool_dib':
+    ensure  => directory,
+    mode    => '0755',
+    owner   => 'nodepool',
+    group   => 'nodepool',
+    require => User['nodepool'],
+  }
+
+  # used for storage of d-i-b cached data
+  file { '/opt/dib_cache':
+    ensure  => directory,
+    mode    => '0755',
+    owner   => 'nodepool',
+    group   => 'nodepool',
+    require => User['nodepool'],
+  }
+
+  # used as TMPDIR during d-i-b image builds
+  file { '/opt/dib_tmp':
+    ensure  => directory,
+    mode    => '0755',
+    owner   => 'nodepool',
+    group   => 'nodepool',
+    require => User['nodepool'],
+  }
+
+  file { '/var/log/nodepool':
+    ensure  => directory,
+    mode    => '0755',
+    owner   => 'nodepool',
+    group   => 'nodepool',
+    require => User['nodepool'],
+  }
+
+  file { '/var/run/nodepool':
+    ensure  => directory,
+    mode    => '0755',
+    owner   => 'nodepool',
+    group   => 'nodepool',
+    require => User['nodepool'],
+  }
+
+}
