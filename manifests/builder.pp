bb12dbb6466e1a53dd7a0c31eca58b04e6795eb8
Spencer Krum <nibz@spencerkrum.com>
2016-01-22 14:05:38 -0800

    Colocate a builder-worker with the service
    This is an intermediate step before builders are completely broken out
    into their own hosts and nodes.
    
    As part of this work we split up the logging configs for the two daemons
    which allows them to be colocated and write to different log files on
    the same host.
    
    Co-Authored-By: Clark Boylan <clark.boylan@gmail.com>
    Change-Id: I990e96dde352fb7d01cc4b89d0f4f02de166b943

---
+++ manifests/builder.pp
@@ -0,0 +1,59 @@
+# Copyright 2015 2015 IBM
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+# == Class: nodepool::builder
+#
+class nodepool::builder(
+  $statsd_host = undef,
+  $image_log_document_root = '/var/log/nodepool/image',
+  $builder_logging_conf_template = 'nodepool/nodepool-builder.logging.conf.erb',
+  $environment = {},
+) {
+
+  file { '/etc/init.d/nodepool-builder':
+    ensure => present,
+    mode   => '0555',
+    owner  => 'root',
+    group  => 'root',
+    source => 'puppet:///modules/nodepool/nodepool-builder.init',
+  }
+
+  file { '/etc/default/nodepool-builder':
+    ensure  => present,
+    content => template('nodepool/nodepool-builder.default.erb'),
+    mode    => '0444',
+    owner   => 'root',
+    group   => 'root',
+  }
+
+  file { '/etc/nodepool/builder-logging.conf':
+    ensure  => present,
+    mode    => '0444',
+    owner   => 'root',
+    group   => 'root',
+    content => template($builder_logging_conf_template),
+  }
+
+  service { 'nodepool-builder':
+    name       => 'nodepool-builder',
+    enable     => true,
+    hasrestart => true,
+    require    => [
+      File['/etc/init.d/nodepool-builder'],
+      File['/etc/default/nodepool-builder'],
+      File['/etc/nodepool/builder-logging.conf'],
+    ],
+  }
+
+}
