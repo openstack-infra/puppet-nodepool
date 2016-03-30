83673823b8df4fb1d3e2f4d6368e65a50e576313
Mateusz Matuszkowiak <mmatuszkowiak@mirantis.com>
2016-03-31 01:07:43 +0200

    Moved mysql part out of init.pp
    - Created nodepool::mysql class, which includes mysql stuff from
      init.pp. This allows to host database on separated host than
      the one running nodepool.
    
    Change-Id: Ic745875b69563ed3834e99533227ca3d1150156b

--- manifests/init.pp
+++ manifests/init.pp
@@ -38,41 +38,6 @@ class nodepool (
   $jenkins_masters = [],
 ) {
 
-
-  $mysql_data = load_module_metadata('mysql', true)
-  if $mysql_data == {} {
-    class { '::mysql::server':
-      config_hash => {
-        'root_password'  => $mysql_root_password,
-        'default_engine' => 'InnoDB',
-        'bind_address'   => '127.0.0.1',
-      }
-    }
-  } else { # If it has metadata.json, assume it's new enough to use this interface
-    class { '::mysql::server':
-      root_password    => $mysql_root_password,
-      override_options => {
-        'mysqld' => {
-          'default-storage-engine' => 'InnoDB',
-        }
-      },
-    }
-  }
-
-  include ::mysql::server::account_security
-
-  mysql::db { 'nodepool':
-    user     => 'nodepool',
-    password => $mysql_password,
-    host     => 'localhost',
-    grant    => ['all'],
-    charset  => 'utf8',
-    require  => [
-      Class['mysql::server'],
-      Class['mysql::server::account_security'],
-    ],
-  }
-
   $packages = [
     'build-essential',
     'libffi-dev',
